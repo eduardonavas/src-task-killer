6c5e68a1a062b4486cff7f3af6870dcb0e832a27
eduardonavas <eduardo.rnavask@gmail.com>
2022-04-13 21:31:23 -0300

    ajuste

--- src/main/java/br/com/navas/application/service/ConsultProcess.java
+++ src/main/java/br/com/navas/application/service/ConsultProcess.java
@@ -1,6 +1,7 @@
 package br.com.navas.application.service;
 
 import br.com.navas.framework.adapter.cmd.out.ExecuteCommand;
+import br.com.navas.framework.helper.Constants;
 
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class ConsultProcess {
 
     public String run(List<String> linhas){
 
-        String resultado = linhas.stream().filter(l -> l.contains("java.exe")).findFirst().orElseThrow(() -> new RuntimeException("Nenhum processo java encontrado"));
+        String resultado = linhas.stream().filter(l -> l.contains(Constants.JAVA_EXE)).findFirst().orElseThrow(() -> new RuntimeException("Nenhum processo java encontrado"));
         int idxOfConsole = resultado.indexOf("Console");
         resultado = resultado.substring(idxOfConsole - 10, idxOfConsole);
 
---
+++ src/main/java/br/com/navas/framework/helper/Constants.java
@@ -0,0 +1,5 @@
+package br.com.navas.framework.helper;
+
+public class Constants {
+    public static final String JAVA_EXE = "java.exe";
+}
